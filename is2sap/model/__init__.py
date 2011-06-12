1915e808bd39b2a0086cb301444c1d2745e654f2
Raul Benitez <raul_kvd@hotmail.com>
2011-06-12 17:46:53 -0400

    cambios menores2

--- is2sap/model/__init__.py
+++ is2sap/model/__init__.py
@@ -59,7 +59,7 @@ def init_model(engine):
 # Import your model modules here.
 from is2sap.model.model import Usuario, Rol, Permiso
 from is2sap.model.model import Proyecto, EstadoFase, Fase, TipoItem, Item, LineaBase, Atributo, ItemDetalle
-from is2sap.model.model import ItemHistorial, ItemDetalleHistorial, HistorialItem
+from is2sap.model.model import ItemHistorial, ItemDetalleHistorial
 
 
 
