0e43bfe5eec0a9d4e0e61fb674236e3912549282
Alberto Garcete <albertogarcetepy@gmail.com>
2011-06-20 14:47:25 -0400

    relaciones padre-hijo

--- is2sap/controllers/relacion_controlador.py
+++ is2sap/controllers/relacion_controlador.py
@@ -1,28 +1,32 @@
 # -*- coding: utf-8 -*-
 """Controlador de Relacion"""
 
-from tg import expose, flash, require, url, request, redirect
-from pylons.i18n import ugettext as _, lazy_ugettext as l_
-from tgext.admin.tgadminconfig import TGAdminConfig
-from tgext.admin.controller import AdminController, AdminConfig
+from tg import expose, flash, require, redirect
 from repoze.what import predicates
-from tg import tmpl_context, validate
+from tg import validate
 from webhelpers import paginate
 
 from is2sap.lib.base import BaseController
 from is2sap.model import DBSession, metadata
 from is2sap.model.model import RelacionItem, Fase, TipoItem, Item, Proyecto
 from is2sap import model
-from is2sap.controllers.secure import SecureController
-from is2sap.controllers.error import ErrorController
+from is2sap.controllers.grafo.grafo import Graph
+import transaction
+import time
+import os
 
+from is2sap.widgets.relacion_form import editar_relacion_form
+
+newGrafo = Graph()
+inserciones=0
 
 
 __all__ = ['RelacionController']
 
 
 class RelacionController(BaseController):
-
+    
+    
     @expose()
     def index(self):
         """Muestra la pantalla inicial"""
@@ -30,8 +34,11 @@ class RelacionController(BaseController):
 
 
     @expose('is2sap.templates.relacion.nuevo')
-    def nuevo(self, idItemActual):
+    def nuevo(self, idItemActual, id_proyecto, id_fase, id_tipo_item):
         """Establece una relacion para el item."""
+        global inserciones
+        inserciones = 0
+        print "el numero de inserciones es :", inserciones
         faseActual=DBSession.query(Fase).join(TipoItem).join(Item).filter(Item.id_item==idItemActual).one()
         itemsDeFase = DBSession.query(Item).join(TipoItem).join(Fase).filter(Fase.id_fase==faseActual.id_fase).filter(Item.id_item!=idItemActual).all()
         hijos = DBSession.query(RelacionItem).filter_by(id_item1=idItemActual).filter_by(tipo="Padre-Hijo").order_by(RelacionItem.id_item2).all()
@@ -46,51 +53,105 @@ class RelacionController(BaseController):
         for fase in proyecto.fases:
             print fase.id_fase
             if fase.id_fase==faseActual.id_fase:                
-                posicion = proyecto.fases.index(fase) - 1
-        itemsDeFaseAdyacente = DBSession.query(Item).join(TipoItem).join(Fase).filter(Fase.id_fase==proyecto.fases[posicion].id_fase).all()
+                posicion = proyecto.fases.index(fase) - 1                
+        if posicion >= 0:
+            itemsDeFaseAdyacente = DBSession.query(Item).join(TipoItem).join(Fase).filter(Fase.id_fase==proyecto.fases[posicion].id_fase).all()
+        else:
+            itemsDeFaseAdyacente = DBSession.query(Item).join(TipoItem).join(Fase).filter(Item.descripcion=="").all()
         antecesores = DBSession.query(RelacionItem).filter_by(id_item2=idItemActual).filter_by(tipo="Antecesor-Sucesor").order_by(RelacionItem.id_item1).all()       
         for antec in antecesores:        
             for item2 in itemsDeFaseAdyacente:            
                 if item2.id_item == antec.id_item1:
                     itemsDeFaseAdyacente.remove(item2)
-        return dict(nombre_modelo='RelacionItem', page='relacion', idItemActual=idItemActual, itemsDeFase=itemsDeFase, itemsDeFaseAdyacente=itemsDeFaseAdyacente)
+        return dict(nombre_modelo='Relaciones', page='relacion', idItemActual=idItemActual, itemsDeFase=itemsDeFase, itemsDeFaseAdyacente=itemsDeFaseAdyacente, id_proyecto=id_proyecto, id_fase=id_fase, id_tipo_item=id_tipo_item)
 
     @expose("is2sap.templates.relacion.listado")
-    def listado(self, id_item):
+    def listado(self, id_item, id_proyecto, id_fase, id_tipo_item):
         """Metodo para listar todos los usuarios de la base de datos"""
         antecesores = DBSession.query(RelacionItem).filter_by(id_item2=id_item).filter_by(tipo="Antecesor-Sucesor").order_by(RelacionItem.id_item1)
         hijos = DBSession.query(RelacionItem).filter_by(id_item1=id_item).filter_by(tipo="Padre-Hijo").order_by(RelacionItem.id_item2)
-        return dict(antecesores=antecesores, hijos=hijos, idItemActual=id_item)
+        return dict(antecesores=antecesores, hijos=hijos, idItemActual=id_item, id_proyecto=id_proyecto, id_fase=id_fase, id_tipo_item=id_tipo_item)
 
+    
+    def buscarCiclos(self, item):    
+        padres = DBSession.query(RelacionItem).filter_by(id_item2=item).filter_by(tipo="Padre-Hijo").order_by(RelacionItem.id_item1)
+        for padre in padres:
+            print ":::: ", padre.id_item1, padre.id_item2
+            newGrafo.add_edge(padre.id_item1, padre.id_item2, False)            
+            self.buscarCiclos(padre.id_item1)
+
+    def insertar(self, kw): 
+        newGrafo.__init__()
+        global inserciones
+        inserciones = inserciones + 1
+        print "el numero de inserciones es :", inserciones
+        if inserciones == 1:                        
+            self.buscarCiclos(kw['id_item1'])
+            id_item1 = int(kw['id_item1'])
+            id_item2 = int(kw['id_item2'])
+            newGrafo.add_edge(id_item1, id_item2, False)
+            hayciclo = newGrafo.walk(id_item1)
+            print newGrafo
+            print hayciclo
+            if hayciclo:
+                print "La insercion de esta relacion creara un ciclo"
+            else:
+                print "No se encuentran ciclos"        
+            
+                relacion = RelacionItem()
+                relacion.tipo = "Padre-Hijo"
+                relacion.id_item1 = kw['id_item1']
+                relacion.id_item2 = kw['id_item2']
+                #relacion.estado = "Activo"
+                #relacion.version = version        
+                DBSession.add(relacion)
+                DBSession.flush() 
+                transaction.commit()        
+    @expose()
+    def addHijo(self, **kw):
+        self.insertar(kw)
+        """listaRelacion=[]
+        newGrafo.__init__()
+        print listaRelacion
+    
+        if len(listaRelacion) == 0:                        
+            self.buscarCiclos(kw['id_item1'])
+            id_item1 = int(kw['id_item1'])
+            id_item2 = int(kw['id_item2'])
+            newGrafo.add_edge(id_item1, id_item2, False)
+            hayciclo = newGrafo.walk(id_item1)
+            print newGrafo
+            print hayciclo
+            if hayciclo:
+                print "La insercion de esta relacion creara un ciclo"
+            else:
+                print "No se encuentran ciclos"        
+            
+                relacion = RelacionItem()
+                relacion.tipo = "Padre-Hijo"
+                relacion.id_item1 = kw['id_item1']
+                relacion.id_item2 = kw['id_item2']
+                #relacion.estado = "Activo"
+                #relacion.version = version        
+                DBSession.add(relacion)
+                DBSession.flush() 
+                transaction.commit()"""       
+        redirect('listado', id_item=kw['id_item1'], id_proyecto=kw['id_proyecto'], id_fase=kw['id_fase'], id_tipo_item=kw['id_tipo_item'])
+        
 
 
     @expose()
-    def addHijo(self, id_item1, id_item2):
+    def addAncestro(self, **kw):
         """Metodo para agregar un registro a la base de datos """       
-        relacion = RelacionItem()
-        relacion.tipo = "Padre-Hijo"
-        relacion.id_item1 = id_item1
-        relacion.id_item2 = id_item2
-        #relacion.estado = "Activo"
-        #relacion.version = version        
-        DBSession.add(relacion)
-        DBSession.flush()
-        redirect("/relacion/listado", id_item=id_item1)
-
-
-    @expose()
-    def addAncestro(self, id_item1, id_item2):
-        """Metodo para agregar un registro a la base de datos """       
-        print "Estoy Creando Una relacion"
         relacion1 = RelacionItem()
         relacion1.tipo = "Antecesor-Sucesor"        
-        relacion1.id_item1 = id_item1
-        relacion1.id_item2 = id_item2
+        relacion1.id_item1 = kw['id_item1']
+        relacion1.id_item2 = kw['id_item2']
         ##relacion1.estado = "Activo"
         ##relacion1.version = version        
         DBSession.add(relacion1)
         DBSession.flush()       
-        redirect("/relacion/listado", id_item=id_item2)
+        redirect('listado', id_item=kw['id_item2'], id_proyecto=kw['id_proyecto'], id_fase=kw['id_fase'], id_tipo_item=kw['id_tipo_item'])
 
 #    @expose('is2sap.templates.usuario.editar')
 #    def editar(self, id_usuario, **kw):
@@ -110,7 +171,7 @@ class RelacionController(BaseController):
 #        return dict(nombre_modelo='Usuario', page='eliminar_usuario', value=usuario)
 
     @expose()
-    def delete(self, id_relacion, idItemActual, **kw):
+    def delete(self, id_relacion, idItemActual, id_proyecto, id_fase, id_tipo_item):
        """Metodo que elimina un registro de la base de datos"""
        DBSession.delete(DBSession.query(RelacionItem).get(id_relacion))
-       redirect("/relacion/listado", id_item=idItemActual)
+       redirect('/relacion/listado', id_item=idItemActual, id_proyecto=id_proyecto, id_fase=id_fase, id_tipo_item=id_tipo_item)
